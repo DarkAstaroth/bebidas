e79f9838e01f3596ef86a210fb637f8ffdd7a260
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-05 17:58:21 -0400

    creando el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Formulario = () => {
     return ( 
-        <h1>Formulario aqui</h1>
+       <form 
+            className="col-12"
+        >
+           <fieldset className="text-center">
+               <legend>Bebidas por categoria o ingrediente</legend>
+           </fieldset>
+
+           <div className="row mt-4">
+               <div className="col-md-4">
+                   <input 
+                        name="nombre"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por ingredientes"
+                    />
+               </div>
+
+               <div className="col-md-4">
+                   <select 
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">--Selecciones Categoria--</option>
+                   </select>
+               </div>
+
+               <div className="col-md-4">
+                   <input 
+                        type="submit" 
+                        value="Buscar bebidas" 
+                        className="btn btn-primary btn-block"/>
+               </div>
+           </div>
+       </form>
      );
 }
  
