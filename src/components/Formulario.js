bcd1fd43b774cbf2d4641847c4251992024aacf4
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-06 23:39:22 -0400

    mostrando las opciones del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -29,6 +29,14 @@ const Formulario = () => {
                         name="categoria"
                     >
                         <option value="">--Selecciones Categoria--</option>
+                        {
+                            categorias.map(categoria =>(
+                               <option 
+                                key={categoria.strCategory}
+                                value={categoria.strCategory}
+                               >{categoria.strCategory}</option> 
+                            ))
+                        }
                    </select>
                </div>
 
