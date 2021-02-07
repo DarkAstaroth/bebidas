165d0e4bb7d95102d146c383f9b210886b8d7e6e
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-07 15:19:18 -0400

    Consultando a la API para obtener la receta en especifico

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -8,6 +8,21 @@ const ModalProvider = (props) => {
     // state del provider
 
     const [idReceta, setIdReceta] = useState(null);
+    const [receta, setReceta] = useState({});
+
+    // una vez tenemos una receta, llmar a la api
+
+    useEffect(() => {
+        const obtenerReceta = async () =>{
+            if(!idReceta) return;
+            
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+            const resultado = await axios.get(url);
+            //console.log(resultado.data.drinks[0]);
+            setReceta(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+    }, [idReceta])
 
     
     return ( 
